b7b03196601a7956d85380562a79f4053f4949d7
Koenraad Verheyden <github@kvrhdn.dev>
2020-08-29 12:21:28 +0200

    Refactor tferun API and implementation
    - Optional options are pointers to be able to distinguish unset fields
    - Directory defaults to `./`
    - Retry every 1 second instead of every 100 milliseconds

--- cmd/main.go
+++ cmd/main.go
@@ -10,15 +10,14 @@ import (
 )
 
 type options struct {
-	// TODO we shouldn't pass the token as command line argument
-	Token             string `short:"t" long:"token" required:"true" description:"Terraform Cloud token"`
-	Organization      string `short:"o" long:"organization" required:"true" description:"The organization on Terraform Cloud."`
-	Workspace         string `short:"w" long:"workspace" required:"true" description:"The workspace on Terraform Cloud."`
-	Message           string `short:"m" long:"message" default:"Created using go-tfe-run" description:"Message to use as name of the run."`
-	Directory         string `short:"d" long:"directory" default:"./" description:"The directory that is uploaded to Terraform Cloud, respects .terraformignore."`
-	Speculative       bool   `long:"speculative" description:"Whether to create a speculative run. A speculative run can not be applied."`
-	WaitForCompletion bool   `long:"wait" description:"Whether we should wait for the non-speculative run to be applied. This will block until the run is finished."`
-	TfVars            string `long:"tf-vars" description:"Contents of a auto.tfvars file that will be uploaded to Terraform Cloud. This can be used to set Terraform variables. These variables will not be persisted."`
+	Token             string  `short:"t" long:"token" required:"true" description:"Terraform Cloud token"`
+	Organization      string  `short:"o" long:"organization" required:"true" description:"The organization on Terraform Cloud."`
+	Workspace         string  `short:"w" long:"workspace" required:"true" description:"The workspace on Terraform Cloud."`
+	Message           *string `short:"m" long:"message" description:"Message to use as name of the run."`
+	Directory         *string `short:"d" long:"directory" default:"./" description:"The directory that is uploaded to Terraform Cloud, respects .terraformignore."`
+	Speculative       bool    `long:"speculative" description:"Whether to create a speculative run. A speculative run can not be applied."`
+	WaitForCompletion bool    `long:"wait" description:"Whether we should wait for the non-speculative run to be applied. This will block until the run is finished."`
+	TfVars            *string `long:"tf-vars" description:"Contents of a auto.tfvars file that will be uploaded to Terraform Cloud. This can be used to set Terraform variables. These variables will not be persisted."`
 }
 
 func main() {
@@ -46,7 +45,7 @@ func main() {
 	_, err = tferun.Run(ctx, runOptions)
 
 	if err != nil {
-		fmt.Printf("Error: run failed: %v", err)
+		fmt.Printf("Error: run failed: %v\n", err)
 		os.Exit(1)
 	}
 }
---
+++ type_helpers.go
@@ -0,0 +1,4 @@
+// String returns a pointer to the given string.
+func String(s string) *string {
+	return &s
+}
